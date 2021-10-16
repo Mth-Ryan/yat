e5c2dfa8857486ce0f9731453247ad1471da3fa2
Mateus Ryan <mthryan@protonmail.com>
2021-10-16 18:31:47 -0300

    Add colors.lua

---
+++ lua/yat/colors.lua
@@ -0,0 +1,37 @@
+--[[
+
+__   __    _
+\ \ / /_ _| |_
+ \ V / _` | __|
+  | | (_| | |_
+  |_|\__,_|\__|
+
+
+File:        colors.lua
+Author:      Mateus Ryan <mthryan@protonmail.com>
+Description: Yes, another theme
+Licence:     MIT
+
+--]]
+
+local colors = {
+    bg1 = "#0B0C0E",
+    fg1 = "#D4D2CF",
+    fg2 = "#c3c1be",
+    fg3 = "#b2b0ae",
+    fg4 = "#a1a09d",
+    bg2 = "#151518",
+    bg3 = "#222226",
+    bg4 = "#303036",
+    ora = "#fd9149",
+    pur = "#A682B0",
+    red = "#BF5151",
+    gra = "#65676a",
+    gre = "#bfd066",
+    blu = "#7CA5BF",
+    cya = "#9BC5AF",
+    err = "#c13535",
+    war = "#f0c674",
+}
+
+return colors
